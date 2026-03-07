acd71f4524d763566dec2aec4549e662fa97378d
Adiba Imran <adibaimran@Adibas-MacBook-Air.local>
2026-03-07 23:00:34 +0530

    UC3

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,40 @@
 /**
-* OOPSBannerApp UC1 - 0OPS Banner Display Application
+* OOPSBannerApp UC3- 0OPS Banner Display Application
 * This class demonstrates a simple Java application that displays the Object
 * Oriented Programming System OOPS acronym to the console.
 *
 * @author Adiba Imran
-* @version 1.0
+* @version 3.0
 */
 
 public class OOPSBannerApp {
 	public static void main(String[] args) {
-		System.out.println("OOPS");
+		System.out.println(" *** " + "   " + " *** " + "   " + "**** " + "   " + " ****");
+		System.out.println("*   *" + "   " + "*   *" + "   " + "*   *" + "   " + "*    ");
+		System.out.println("*   *" + "   " + "*   *" + "   " + "*   *" + "   " + "*    ");
+		System.out.println("*   *" + "   " + "*   *" + "   " + "**** " + "   " + " *** ");
+		System.out.println("*   *" + "   " + "*   *" + "   " + "*    " + "   " + "    *");
+		System.out.println("*   *" + "   " + "*   *" + "   " + "*    " + "   " + "    *");
+		System.out.println(" *** " + "   " + " *** " + "   " + "*    " + "   " + "**** ");
+		System.out.println(String.join(" ", 
+        " *** ", " *** ", " **** ", " ****"));
+
+System.out.println(String.join(" ", 
+        "*   *", "*   *", "*   *", "*    "));
+
+System.out.println(String.join(" ", 
+        "*   *", "*   *", "*   *", "*    "));
+
+System.out.println(String.join(" ", 
+        "*   *", "*   *", " **** ", " *** "));
+
+System.out.println(String.join(" ", 
+        "*   *", "*   *", "*     ", "    *"));
+
+System.out.println(String.join(" ", 
+        "*   *", "*   *", "*     ", "    *"));
+
+System.out.println(String.join(" ", 
+        " *** ", " *** ", "*     ", " ****"));
 	}
 }
